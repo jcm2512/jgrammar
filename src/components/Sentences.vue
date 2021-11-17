4188adff3ea942fb2a6d604d7d8be8211ed3e6e2
jcm2512 <joe.magalotti@gmail.com>
2021-11-18 03:34:49 +0900

    import from grammarwise

---
+++ src/components/Sentences.vue
@@ -0,0 +1,151 @@
+<template>
+  <button v-on:click="toggleEditMode">{{ editMode ? "Done" : "Edit" }}</button>
+  <ul>
+    <li v-for="(obj, index) in grammarList" :key="`obj-${index}`">
+      {{ Object.keys(obj)[0]
+      }}<a href="#" v-show="editMode" v-on:click="remove(obj)">[x]</a>
+      <ul>
+        <li v-for="(sentence, index) in Object.values(obj)[0]" :key="`sentence-${index}`">
+          <div>{{ sentence }}</div>
+          <a href="#" v-show="editMode" v-on:click="clear(obj, sentence)"
+            >[x]</a
+          >
+        </li>
+      </ul>
+    </li>
+  </ul>
+
+  <input
+    v-if="findMode"
+    placeholder="Search Grammar"
+    v-on:keyup.enter="findGrammar"
+  />
+  <input
+    v-if="editMode"
+    placeholder="Add Grammar"
+    v-on:keyup.enter="addGrammar"
+  />
+  <div>
+    <br /><button v-if="editMode" v-on:click="getGrammar(grammarList)">
+      Get Grammar Points
+    </button>
+  </div>
+
+  <button v-on:click="toggleAllSentences">
+    {{ showAll ? "Hide All Sentences" : "Show All Sentences" }}
+  </button>
+  <ul>
+    <li v-for="(sentence, index) in allSentences" v-show="showAll" :key="`s-${index}`">
+      <div>{{ sentence }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Sentences",
+  data() {
+      return{
+    editMode: false,
+    findMode: true,
+    showAll: true,
+    grammarList: [{ 恐らく: [] }, { に際して: [] }],
+    allSentences: [],
+      }
+  },
+  methods: {
+    findGrammar: function (event) {
+      let sentence = event.target.value;
+      let grammar = this.searchGrammar(sentence);
+      grammar.length < 1
+        ? "Nothing found"
+        : this.addSentence(grammar, sentence);
+      event.target.value = "";
+      this.save();
+    },
+    updateSentence: function (array, sentence) {
+      let isUnique = true;
+      array.forEach((s) => {
+        if (s === sentence) {
+          isUnique = false;
+        }
+      });
+      if (isUnique) array.push(sentence);
+    },
+    addSentence: function (grammar, sentence) {
+      grammar.forEach((g) => {
+        this.grammarList.forEach((obj) => {
+          if (Object.keys(obj)[0] == g) {
+            this.updateSentence(Object.values(obj)[0], sentence);
+            this.updateSentence(this.allSentences, sentence);
+          }
+        });
+      });
+    },
+    getGrammar: function (array) {
+      let list = [];
+      array.forEach((obj) => list.push(Object.keys(obj)[0]));
+      return list;
+    },
+    addGrammar: function (event) {
+      this.grammarList.push({
+        [event.target.value]: [],
+      });
+      event.target.value = "";
+      this.save();
+    },
+    searchGrammar: function (s) {
+      let output = [];
+      this.getGrammar(this.grammarList).forEach((item) => {
+        let result = [];
+        let grammar = item.split("～");
+        grammar.forEach((grammarPart) => {
+          let found = s.search(grammarPart);
+          if (found > -1)
+            result.push(s.substring(found, found + Element.length));
+        });
+        if (result.length === grammar.length) output.push(item);
+      });
+      return output;
+    },
+    remove: function (f) {
+      this.grammarList = this.grammarList.filter((i) => i != f);
+      this.save();
+    },
+    clear: function (obj, v) {
+      let array = Object.values(obj)[0];
+      array.splice(
+        array.findIndex((i) => i == v),
+        1
+      );
+    },
+    save: function () {
+      let data = JSON.stringify(this.grammarList);
+      let datatwo = JSON.stringify(this.allSentences);
+      localStorage.setItem("saved", data);
+      localStorage.setItem("savedtwo", datatwo);
+    },
+    load: function () {
+      let data = localStorage.getItem("saved");
+      let datatwo = localStorage.getItem("savedtwo");
+      if (data) {
+        this.grammarList = JSON.parse(data);
+      }
+      if (datatwo) {
+        this.allSentences = JSON.parse(datatwo);
+      }
+    },
+    toggleEditMode: function () {
+      this.editMode = !this.editMode;
+      this.findMode = !this.findMode;
+      this.save();
+    },
+    toggleAllSentences: function () {
+      this.showAll = !this.showAll;
+    },
+  },
+  created: function () {
+    this.load();
+  },
+};
+</script>
